c30a412ff45a07f5444a30787b2eba2d0211f7ac
tejas <pateltejas.2005@gmail.com>
2023-04-19 23:37:34 +0530

    changes

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const url = 'mongodb://localhost:27017/inotebook'
+const url = 'mongodb://0.0.0.0:27017/inotebook'
 
 const connectToMongo = () => {
     mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
